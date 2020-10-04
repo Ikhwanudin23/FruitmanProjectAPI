1814927f9d51cd9ee84f294ae750a0fef411858e
ikhwanudin23 <ikhwanudin2309@gmail.com>
2020-10-04 17:25:42 +0700

    json

--- app/Http/Resources/User/ProductResource.php
+++ app/Http/Resources/User/ProductResource.php
@@ -16,11 +16,14 @@ class ProductResource extends JsonResource
     {
         return [
             "id" => $this->id,
+            "name" => $this->fruit->name,
             "description" => $this->description,
             "image" => $this->image,
             "address" => $this->address,
             "price" => $this->price,
             "status" => $this->status,
+            "latitude" => $this->latitude,
+            "longitude" => $this->longitude,
             "seller" => new SellerResource($this->seller),
             "fruit" => new FruitResource($this->fruit),
             "sub_district" => new SubDistrictResource($this->subDistrict)
